80bddaa8dd5a057f424791297309f815b72fdefa
hi-hee <vitality710@naver.com>
2020-12-14 08:11:20 +0900

    Update 5-securitygroups.sh

--- deploymentTests/5-securitygroups.sh
+++ deploymentTests/5-securitygroups.sh
@@ -2,55 +2,7 @@
 
 echo "securitygourps.sh1"
 go get -u github.com/jvehent/pineapple
-echo "securitygourps.sh"
-$GOPATH/bin/pineapple -c << EOF
-aws:
-    region: us-east-2
-    accountnumber: 998267510686
+echo "securitygourps.sh2"
+$GOPATH/bin/pineapple -c /app/deploymentTests/SG.yaml
 
-components:
-    - name: load-balancer
-      type: elb
-      tag:
-          key: elasticbeanstalk:environment-name
-          value: Invoicer02-env
-
-    - name: application
-      type: ec2
-      tag: 
-          key: elasticbeanstalk:environment-name
-          value: Invoicer02-env
-
-    - name: database
-      type: rds
-      tag:
-          key: environment-name
-          value: Invoicer02-env
-
-#    - name: bastion
-#      type: ec2
-#      tag:
-#          key: environment-name
-#          value: invoicer-bastion
-
-rules:
-    - src: 0.0.0.0/0
-      dst: load-balancer
-      dport: 443
-
-    - src: load-balancer
-      dst: application
-      dport: 80
-
-    - src: application
-      dst: database
-      dport: 5432
-
-#    - src: bastion
-#      dst: application
-#      dport: 22
-#
-#    - src: bastion
-#      dst: database
-#      dport: 5432
-EOF
+echo "securitygourps.sh2"
