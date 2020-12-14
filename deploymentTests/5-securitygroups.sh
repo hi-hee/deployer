90048f41d92db5c94377717949fdfe75bc7cd832
hi-hee <vitality710@naver.com>
2020-12-14 11:28:28 +0900

    Update 5-securitygroups.sh

--- deploymentTests/5-securitygroups.sh
+++ deploymentTests/5-securitygroups.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env sh
 
-echo "securitygourps.sh1"
 go get -u github.com/jvehent/pineapple
-echo "securitygourps.sh2"
 $GOPATH/bin/pineapple -c /app/deploymentTests/SG.yaml
-
-echo "securitygourps.sh2"
